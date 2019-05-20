9e4d255163ed597550784793b2f585571a22ccfe
Luke <f74178qazqwe@yahoo.com.tw>
2019-05-21 01:13:39 +0800

    message

--- src/components/HelloWorld2.vue
+++ src/components/HelloWorld2.vue
@@ -7,12 +7,16 @@
         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
       </div>
 
+      <h1>{{ msg }}</h1>
+
       <div class="input-group mb-3">
         <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
         <div class="input-group-append">
           <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
         </div>
       </div>
+
+      <h1>{{ msg }}</h1>
 
       <div class="form-group">
         <label for="exampleInputPassword1">Password</label>
@@ -25,21 +29,16 @@
       <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 
-    <h1>{{ msg }}</h1>
-    <div class="media">
+    <div class="media" v-for="item in messages" :key="item.username">
       <img src="http://lorempixel.com/100/100/" class="align-self-start mr-3" alt="...">
       <div class="media-body">
-        <h5 class="mt-0">Top-aligned media</h5>
-        <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-        <p>Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+        <h5 class="mt-0">{{item.username}}</h5>
       </div>
     </div>
     <div class="media">
       <img src="http://lorempixel.com/100/100/" class="align-self-end mr-3" alt="...">
       <div class="media-body">
         <h5 class="mt-0">Bottom-aligned media</h5>
-        <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-        <p class="mb-0">Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
       </div>
     </div>
   </div>
@@ -50,7 +49,17 @@ export default {
   name: 'HelloWorld2',
   data () {
     return {
-      msg: 'HelloWorld2'
+      msg: 'HelloWorld2',
+      messages: [
+        {
+          username: 'test',
+          message: 'test'
+        },
+        {
+          username: 'test2',
+          message: 'test2'
+        }
+      ]
     }
   }
 }
